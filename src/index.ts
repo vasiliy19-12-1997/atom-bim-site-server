b9168b51211b714d1ba7a5124ae06721db3f3aa1
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-09-05 16:55:35 +0500

    update proj

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import  express  from "express";
-import cors from 'cors';
+import  { Request, Response } from 'express'
+var express = require('express')
+var cors = require('cors')
+
 const app = express();
 app.use(cors())
 var corsOptions = {
   origin: 'https://atom-bim-site-server.vercel.app/',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-app.get("/", cors(corsOptions), (req, res) => res.send("Express on Vercel"));
+app.get("/", cors(corsOptions), (req:Request, res:Response) => res.send("Express on Vercel"));
 
 app.listen(5001, () => console.log("Server ready on port 5001."));
 
