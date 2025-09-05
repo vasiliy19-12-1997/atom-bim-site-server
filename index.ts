ff131d25ea3cae5ce2b389d313cc15ee249d7497
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-09-05 16:34:47 +0500

    add cors and db

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import  express  from "express";
+import cors from 'cors';
 const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use(cors())
+var corsOptions = {
+  origin: 'https://atom-bim-site-server.vercel.app/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.get("/", cors(corsOptions), (req, res) => res.send("Express on Vercel"));
 
 app.listen(5001, () => console.log("Server ready on port 5001."));
 
